c26aaec153a732169c9c4e3e05cc7a2da8959e1b
ManManu <1910653532@stud.fh-kufstein.ac.at>
2020-04-13 12:21:06 +0200

    login mit der landingpage verbunden

--- includes/config.php
+++ includes/config.php
@@ -4,5 +4,5 @@ define('URL_PATH', '/webdev-project-bildbewertung');
 
 define('LOGIN_URL', 'login');
 define('LOGOUT_URL', 'logout');
-define('INDEX_URL', 'index');
+define('INDEX_URL', 'landingpage.html');
 
