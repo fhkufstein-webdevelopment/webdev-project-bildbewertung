13402ec2919ec3c8d760d406f8beedc6359d342d
ManManu <1910653532@stud.fh-kufstein.ac.at>
2020-04-19 18:00:24 +0200

    logout fertig

--- includes/views/logout.php
+++ includes/views/logout.php
@@ -4,13 +4,14 @@ echo $this->header;
 
 ?>
 
-
+<div class="logoutwrapper">
     <div id="main">
         <div class="row">
-            <h1 class="logouterfolgreich">Logout erfolgreich</h1>
+            <h1 class="logouterfolgreich">Logout erfolgreich!</h1>
             <p class="siesindabgemeldet">Sie sind jetzt abgemeldet. Klicken Sie <a class="wiederanmelden" href="login" style="text-decoration: none;">hier</a> um sich wieder anzumelden.</p>
         </div>
     </div>
+</div>
 <?php
 
 echo $this->footer;
