34f46d5bac2ad86b36886c49c31ed5cd7cf7c100
ManManu <1910653532@stud.fh-kufstein.ac.at>
2020-04-19 22:08:01 +0200

    404 updated

--- includes/views/404.php
+++ includes/views/404.php
@@ -1,24 +1,24 @@
-<?php
-echo $this->header;
-?>
-<div id="main">
-	<h1>404 Seite nicht gefunden</h1>
+<html>
+    <head>
+        <link href="../../css/404.css" rel="stylesheet">
+        <title>404 Seite nicht gefunden</title>
+    </head>
+    <body>
+        <div id="main">
+            <div class="yomo">404 Seite nicht gefunden</div>
 
-	<p>Leider konnte Ihre Anfrage nicht richtig verarbeitet werden. Mögliche Ursachen hierfür sind:</p>
-	<ul>
-		<li>die URL die Sie eingegeben haben enthält einen Tippfehler</li>
-		<li>die Seite die Sie versucht haben aufzurufen existiert nicht mehr</li>
-	</ul>
-	<p>
-		Was können Sie jetzt tun?
-	</p>
-	<ul>
-		<li>prüfen Sie ob die URL tatsächlich richtig ist</li>
-		<li>Nutzen Sie die Navigation um zu der Seite zu gelangen die Sie aufrufen wollten</li>
-	</ul>
-</div>
-<?php
-
-echo $this->footer;
-
-?>
+            <p>Leider konnte Ihre Anfrage nicht richtig verarbeitet werden. Mögliche Ursachen hierfür sind:</p>
+            <ul>
+                <li>die URL die Sie eingegeben haben enthält einen Tippfehler</li>
+                <li>die Seite die Sie versucht haben aufzurufen existiert nicht mehr</li>
+            </ul>
+            <p>
+                Was können Sie jetzt tun?
+            </p>
+            <ul>
+                <li>prüfen Sie ob die URL tatsächlich richtig ist</li>
+                <li>Nutzen Sie die Navigation um zu der Seite zu gelangen die Sie aufrufen wollten</li>
+            </ul>
+        </div>
+    </body>
+</html>
